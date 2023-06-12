a2246c5213e2bb9b426759a89e5228e922f9c6d4
Amit Aryeh Levy <amit@amitlevy.com>
2023-06-12 10:47:20 -0400

    switch to buildRustPackage

--- default.nix
+++ default.nix
@@ -1,3 +1,19 @@
-{}:
+{pkgs ? import <nixpkgs> {} }:
 
-(import ./Cargo.nix {}).rootCrate.build
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "deplorable";
+  version = "0.1.1";
+  src = ./.;
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
+
+  buildInputs = [ pkgs.openssl ];
+  nativeBuildInputs = [ pkgs.pkg-config ];
+
+  meta = {
+    description = "A simple & small daemon to deploy static website and other code from GitHub webhooks";
+    homepage = "https://github.com/alevy/deplorable";
+    license = pkgs.lib.licenses.gpl3;
+  };
+}
--- shell.nix
+++ shell.nix
@@ -1,18 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 
 with pkgs;
-let
-  crate2nix = import (fetchFromGitHub {
-    owner = "kolloch";
-    repo = "crate2nix";
-    rev = "0.8.0";
-    sha256 = "17mmf5sqn0fmpqrf52icq92nf1sy5yacwx9vafk43piaq433ba56";
-  }) {};
-in mkShell {
+mkShell {
   buildInputs = [
     rustc
     cargo
-    crate2nix
     pkgconfig
     openssl
   ];
